da15aaa0d6cf63dde9459777980c65bcee9754f4
Mukesh Singal <mukesh.singal@kony.com>
2018-12-28 16:04:18 +0530

    Adding kony starter project

---
+++ controllers/mobile/Form1Controller/Form1ControllerActions.js
@@ -0,0 +1,11 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for btnLogin **/
+    AS_Button_defbd7b5a92b49a4a1a61992add1f043: function AS_Button_defbd7b5a92b49a4a1a61992add1f043(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("Form2");
+        ntf.navigate();
+    }
+});
